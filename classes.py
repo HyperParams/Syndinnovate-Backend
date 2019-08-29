66f7599f799ca79102f3b11992e40bbaad45dd54
rachitsaksena <rachit.saksena00@gmail.com>
2019-08-29 14:50:13 +0530

    added even more functionality

--- classes.py
+++ classes.py
@@ -1,11 +1,14 @@
 import datetime
 import pickle
-
-#test
+import heapq
 
 def sql_query(mobile_number):
     # TODO: implement SQL query
     pass
+
+with open("resources/weights.bat","rb") as file:
+    weights = pickle.load(file)
+    # TODO: detect for changes in weights
 
 def get_POV():
     with open("resources/POV.bat","rb") as file:
@@ -31,12 +34,17 @@ class customer():
         self.score = self.calculate_score(weights)
         # increasing instances
         instances += 1
+        # variables for internal use
+        self.re_entries = 0
 
     def __del__(self):
         instances -= 1
 
     def __lt__(self, other):
         return self.score > other.score
+
+    def __eq__(self, other):
+        return self.id == other.id
 
     def calculate_score(self, weights):
         POV = get_POV()
@@ -58,3 +66,27 @@ class customer():
     def update_details(self):
         # TODO: update_details
         pass
+
+class line():
+
+    def __init__(self):
+        self.queue = []
+
+    def __push(self, customer):
+        heapq.heappush(self.queue, customer)
+        heapq._heapify_max(self.queue)
+
+    def add_customer(self, customer):
+        self.__push(customer)
+        for i in range(len(self.queue)):
+            if self.queue[i] == customer:
+                l = [i+1:]
+                break
+        self.queue = heapq._heapify_max(self.queue[:i+1])
+
+        for i in range(len(l)):
+            l[i].calculate_score(weights)
+            self.__push(l[i])
+
+    def get_next_customer(self):
+        return heapq._heappop_max
--- flask.py
+++ flask.py
@@ -4,12 +4,33 @@ import flask
 import pickle
 import requests
 
-with open("resources/queue1.bat","rb") as file:
-    queue1 = pickle.load(file)
+re_entry_threshold=3
+
+# TODO: input multiple lines like done for line1
+with open("resources/line1.bat","rb") as file:
+    line1 = classes.line()
+    # line1 = pickle.load(file)
+
+line_assignment = {
+        1:line1,
+        2:line1,
+        3:line1,
+        4:line1,
+        5:line2,
+        6:line2,
+        7:line2,
+        8:line2,
+        9:line3,
+        10:line3,
+        11:line3,
+        12:line3
+}
 
 with open("resources/weights.bat","rb") as file:
     weights = pickle.load(file)
     # TODO: detect for changes in weights
+
+occupied_customers = []
 
 app = flask.Flask(__name__)
 
@@ -21,13 +42,117 @@ def landing():
 def add():
     mobile_number = flask.request.args.get('mobile-number')
     POV = flask.request.args.get('POV')
-    heapq.heappush(queue1, customer(mobile_number, POV, weights))
+    line_assignment[POV].add_customer(customer(mobile_number, POV, weights))
     return flask.Response(), 200
 
-@app.route('/counter-1/empty/', methods=["GET", "POST"]) # TODO: check if multiple slashes work for app route in flask
-def empty1():
-    c = queue1.heappop()
-    r = requests.post(url="<URL>/counter-1/next/",data=vars(c))
-    # TODO: send sms to customer
+# ================================================================================|COUNTER-1|=============================================================================================================
 
-#just test
+@app.route('/counter-1/empty/', methods=["GET", "POST"])
+def empty1():
+    for i in range(len(occupied_customers)):
+        if occupied_customers[i][1] == 1:
+            del occupied_customers[i]
+            break
+    c = line1.get_next_customer()
+    occupied_customers.append((c, 1))
+    r = requests.post(url="<URL>/counter-1/next/", data=vars(c)) # TODO: enter value for <URL>
+    # TODO: send sms to customer
+    return flask.Response(), 200
+
+@app.route('/counter-1/customer-not-there/', methods=["GET", "POST"])
+def cusomer_didnt_show_up1():
+    id = flask.request.args.get('ID')
+    for i in range(len(occupied_customers)):
+        if occupied_customers[i][0].ID == id:
+            c = (occupied_customers.pop(i))[0]
+            break
+    if c.re_entries > re_entry_threshold:
+        del c
+    else:
+        c.re_entries+=1
+        line1.add_customer(c)
+    empty1()
+
+# ================================================================================|COUNTER-2|=============================================================================================================
+
+@app.route('/counter-2/empty/', methods=["GET", "POST"])
+def empty2():
+    for i in range(len(occupied_customers)):
+        if occupied_customers[i][1] == 2:
+            del occupied_customers[i]
+            break
+    c = line2.get_next_customer()
+    occupied_customers.append((c, 2))
+    r = requests.post(url="<URL>/counter-2/next/",data=vars(c)) # TODO: enter value for <URL>
+    # TODO: send sms to customer
+    return flask.Response(), 200
+
+@app.route('/counter-2/customer-not-there/', methods=["GET", "POST"])
+def cusomer_didnt_show_up2():
+    id = flask.request.args.get('ID')
+    for i in range(len(occupied_customers)):
+        if occupied_customers[i][0].ID == id:
+            c = (occupied_customers.pop(i))[0]
+            break
+    if c.re_entries > re_entry_threshold:
+        del c
+    else:
+        c.re_entries+=1
+        line2.add_customer(c)
+    empty2()
+
+# ================================================================================|COUNTER-3|=============================================================================================================
+
+@app.route('/counter-3/empty/', methods=["GET", "POST"])
+def empty3():
+    for i in range(len(occupied_customers)):
+        if occupied_customers[i][1] == 3:
+            del occupied_customers[i]
+            break
+    c = line3.get_next_customer()
+    occupied_customers.append((c, 3))
+    r = requests.post(url="<URL>/counter-3/next/",data=vars(c)) # TODO: enter value for <URL>
+    # TODO: send sms to customer
+    return flask.Response(), 200
+
+@app.route('/counter-3/customer-not-there/', methods=["GET", "POST"])
+def cusomer_didnt_show_up3():
+    id = flask.request.args.get('ID')
+    for i in range(len(occupied_customers)):
+        if occupied_customers[i][0].ID == id:
+            c = (occupied_customers.pop(i))[0]
+            break
+    if c.re_entries > re_entry_threshold:
+        del c
+    else:
+        c.re_entries+=1
+        line3.add_customer(c)
+    empty3()
+
+# ================================================================================|COUNTER-4|=============================================================================================================
+
+@app.route('/counter-4/empty/', methods=["GET", "POST"]) 
+def empty4():
+    for i in range(len(occupied_customers)):
+        if occupied_customers[i][1] == 4:
+            del occupied_customers[i]
+            break
+    c = line1.get_next_customer()
+    occupied_customers.append((c, 4))
+    r = requests.post(url="<URL>/counter-4/next/",data=vars(c)) # TODO: enter value for <URL>
+    # TODO: send sms to customer
+    return flask.Response(), 200
+
+@app.route('/counter-4/customer-not-there/', methods=["GET", "POST"])
+def cusomer_didnt_show_up4():
+    id = flask.request.args.get('ID')
+    for i in range(len(occupied_customers)):
+        if occupied_customers[i][0].ID == id:
+            c = (occupied_customers.pop(i))[0]
+            break
+    if c.re_entries > re_entry_threshold:
+        del c
+    else:
+        c.re_entries+=1
+        line1.add_customer(c)
+    empty4()
